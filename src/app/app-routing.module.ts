b817ae8568cad9d04b1a3802defdafc03cac701e
sharath <sharath.sg27@gmail.com>
2019-01-27 18:01:32 +0530

    Syllabus module with level-2 (finishing) completed
    Signed-off-by: sharath <sharath.sg27@gmail.com>

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+
+const routes: Routes = [
+  {path: '', redirectTo: '', pathMatch: 'full'},
+  {path: 'syllabus', loadChildren: './modules/syllabus/syllabus.module#SyllabusModule'},
+  {path: 'dashboard', loadChildren: './modules/dashboard/dashboard.module#DashboardModule'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NavbarComponent} from './utils/navbar/navbar.component';
+import {SharedToolsModule} from './modules/shared-tools/shared-tools.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedToolsModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
