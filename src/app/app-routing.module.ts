d6e5e14085bda4e3a4bf45189018a0adcad69f53
Sharath S <sharath.sg27@gmail.com>
2019-01-28 15:58:32 +0530

    Default re-routing to syllabus page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 
 const routes: Routes = [
-  {path: '', redirectTo: '', pathMatch: 'full'},
+  {path: '', redirectTo: 'syllabus', pathMatch: 'full'},
   {path: 'syllabus', loadChildren: './modules/syllabus/syllabus.module#SyllabusModule'},
   {path: 'dashboard', loadChildren: './modules/dashboard/dashboard.module#DashboardModule'}
 ];
