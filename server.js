32ecd119bd33e072ad32d90de8200a4da59b4fcf
sharath <sharath.sg27@gmail.com>
2019-02-16 16:38:55 +0530

    build config fix

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files from dist directory
-app.use(express.static(__dirname + 'dist/cognitron'));
+app.use(express.static(__dirname + 'dist'));
 
 app.get('/*', (req, res) => {
 
-  res.sendFile(path.join(__dirname+'/dist/cognitron/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
